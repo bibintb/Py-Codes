16af643b3ca4e467058fa450e30b109191d965ef
bibin t b <bibintb5@gmail.com>
2021-09-27 14:06:13 +0530

    Created

---
+++ Basic Python Notebooks/asyncio/README.md
@@ -0,0 +1,10 @@
+
+
+### Credit
+
+Original Verion Credited to:
+
+```
+https://github.com/veryacademy/Python-Beginner-Plus-PY002/blob/main/001_Async%20IO.md
+```
+
---
+++ Basic Python Notebooks/asyncio/Synchronous.py
@@ -0,0 +1,17 @@
+def task1():
+    print('send first email')
+    print('send first email reply')
+    task2()
+
+def task2():
+    print('send second email')
+    print('send second email reply')
+    task2()
+
+def task3():
+    print('send third email')
+    print('send third email reply')
+    print('==')
+    print('end')
+
+task1()
