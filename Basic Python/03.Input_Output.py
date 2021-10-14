bed40653937aaa0e061111eb9e30eb095d4abdb5
bibin <bibintb5@gmail.com>
2021-10-14 17:54:42 +0530

    updated

---
+++ Basic Python/03.Input_Output.py
@@ -0,0 +1,10 @@
+print("this is a sentance which will presented on the screen")
+
+a = 5
+b = "Jack"
+
+print("Name: \t", b)
+print("Age:\t", a)
+a = input("Nice to meet you\nthank you")
+
+print(a)
---
+++ Basic Python/06.Operators.py
@@ -0,0 +1,81 @@
+# + Operator 
+x = 1
+y = 2
+z = 3
+result = x + y + z
+print(result)
+
+# Concatenate two strings 
+first_name = 'Jack'
+last_name = 'Smith'
+name = first_name + " " + last_name
+print(name)
+
+# - Operator 
+
+a = 23
+b = 4
+result = a - b
+print(result)
+
+a = 12.3
+b = 3
+result = a - b
+print(result)
+
+# * Operator
+a = 23
+b = 4
+result = a * b
+print(result)
+
+a = 23.45
+b = 4
+result = a * b
+print(result)
+
+# Multiply string
+a  = 'zoo'
+b = a * 3
+print(b)
+
+# / Operator and // Operator 
+result = 12/5
+print(result)
+
+result = 12//5
+print(result)
+
+# % Operator 
+
+result = 13%4
+print(result)
+
+# ** Operator 
+result = 2**4
+print(result)
+
+# Assignment operator 
+a = 4
+name = 'Jack Smith'
+
+# Example: = operator 
+x = 133
+x = x + 4
+print(x)
+
+a,b,c = 4, 3.4, 'hello'
+d = e = f = 'how are you'
+print(a,b,c,d,e,f)
+
+x = 12
+x += 23
+print(x)
+
+x = 10
+x //=3
+print(x)
+
+# Operatot precendence 
+print(13 - 2 * 3)
+print((13 - 2) * 3)
